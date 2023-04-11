5c011455d03068376ebc9f3cb5872e58fc2c906c
Kartavya422 <98406815+Kartavya422@users.noreply.github.com>
2023-04-11 16:23:02 +0530

    Delete core directory

--- core/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class DocumentAccessPermission(permissions.BasePermission):
-    """
-    Global permission to check if user has access to the object
-    """
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
